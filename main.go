f815904114a790b244b85a919656154d4ec64bdc
Vito Castellano <castellano.vito@gmail.com>
2019-07-13 00:59:17 +0200

    changed structure

--- main.go
+++ main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
 )
 
-func init() {
-	Info("Init commands package - OK")
-}
+var (
+	bot *tgbotapi.BotAPI
 
-// HomeReplyKeyboard home-keyboard
-var homeReplyKeyboard = tgbotapi.NewReplyKeyboard(
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("Raccontami chi sei"),
-	),
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("Percorso lavorativo"),
-		tgbotapi.NewKeyboardButton("Tecnologie e Progetti"),
-	),
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("Contatti"),
-	),
+	homeReplyKeyboard = tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Raccontami chi sei"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Percorso lavorativo"),
+			tgbotapi.NewKeyboardButton("Tecnologie e Progetti"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Contatti"),
+		),
+	)
 )
 
 // Message - export
@@ -34,7 +39,6 @@ type Message struct {
 }
 
 func consumeChainMessage(structure Message) {
-
 	switch structure.MsgType {
 	case "Message":
 		var response tgbotapi.MessageConfig = tgbotapi.NewMessage(structure.ChatID, structure.Content)
@@ -64,6 +68,89 @@ func consumeChainMessage(structure Message) {
 
 	time.Sleep(structure.Duration * time.Second)
 
+}
+
+func updatesHandler() {
+	var telegramApikey string
+
+	errDotEnv := godotenv.Load()
+	if errDotEnv != nil {
+		log.Fatal("Error loading .env file")
+	}
+	telegramApikey = os.Getenv("TELEGRAM_APIKEY")
+
+	var err error
+	bot, err = tgbotapi.NewBotAPI(telegramApikey)
+	bot.Debug = true
+
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	logMessage := fmt.Sprintf("Bot connesso correttamente %s", bot.Self.UserName)
+	log.Println(logMessage)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, chanErr := bot.GetUpdatesChan(u)
+	if chanErr != nil {
+		log.Panicln(chanErr)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		commandsHandler(update)
+	}
+}
+
+func commandsHandler(update tgbotapi.Update) {
+	command, _, ok := breakCommand(update.Message.Text)
+	if ok {
+		switch command {
+		case "Torna":
+			TornaCommand(update)
+		case "Start":
+			StartCommand(update)
+		case "/Start":
+			StartCommand(update)
+		case "/start":
+			StartCommand(update)
+		case "Raccontami":
+			StoryCommand(update)
+		case "Percorso":
+			JobsCommand(update)
+		case "Tecnologie":
+			TechCommand(update)
+		case "Contatti":
+			ContactsCommand(update)
+		}
+	}
+}
+
+// SendMsg - Send telegram message
+func SendMsg(response tgbotapi.Chattable) {
+	if _, err := bot.Send(response); err != nil {
+		log.Panicln(err)
+	}
+}
+
+func breakCommand(message string) (string, []string, bool) {
+	var command []string
+	var arguments []string
+	if message == "" {
+		return "", arguments, false
+	}
+
+	command = strings.Split(message, " ")
+	if len(command) >= 2 {
+		arguments = strings.Split(command[1], ",")
+	}
+
+	return command[0], arguments, true
 }
 
 // StartCommand - Command
@@ -154,4 +241,10 @@ func ContactsCommand(update tgbotapi.Update) {
 	for _, story := range stories {
 		consumeChainMessage(story)
 	}
+}
+
+func main() {
+	// Bot
+	log.Println("Start Bot")
+	updatesHandler()
 }
